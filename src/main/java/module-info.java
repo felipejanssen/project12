3102eada8d4660a6a5e525a610fa8a0f65b1a28e
Omer <96557499+OmericoN@users.noreply.github.com>
2025-03-10 11:15:15 +0100

    Initialized the project structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module project12.application {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens project12.application to javafx.fxml;
+    exports project12.application;
+}
