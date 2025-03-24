8f1cc0c9c32284a2a884222eea9154d271f471c5
Omer <96557499+OmericoN@users.noreply.github.com>
2025-03-24 20:19:23 +0100

    comments added for guidance

--- src/main/java/Backend/ODEsolver.java
+++ src/main/java/Backend/ODEsolver.java
@@ -1,4 +1,6 @@
 package Backend;
+import java.util.function.BiFunction;
+//A bifunction takes time as the first argument, and a current vector of a single planet as a second argument
 
 public class ODEsolver {
     private double[] function;
