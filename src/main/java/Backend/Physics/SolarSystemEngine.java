4aa907d8806b8777ce107da918e2b59f3e1bd5fd
Petar <p.poltoracki@student.maastrichtuniversity.nl>
2025-04-22 16:09:29 +0200

    Implemented fully-coupled solar system physics engine

---
+++ src/main/java/Backend/Physics/SolarSystemEngine.java
@@ -0,0 +1,52 @@
+package Backend.Physics;
+
+import Backend.SolarSystem.CelestialObject;
+import Backend.ODE.ODEsolver;
+import Utils.vec;
+
+import java.util.ArrayList;
+
+public class SolarSystemEngine {
+
+    private ArrayList<CelestialObject> bodies;
+    private ODEsolver solver;
+
+    public SolarSystemEngine(ArrayList<CelestialObject> bodies) {
+        this.bodies = bodies;
+
+        double[] initialStates = new double[bodies.size() * 6];
+        for (int i = 0; i < bodies.size(); i++) {
+            System.arraycopy(bodies.get(i).getState().getState(), 0, initialStates, i * 6, 6);
+        }
+
+        solver = new ODEsolver(SolarSystemODE.create(bodies));
+    }
+
+    public void evolve(double time, double h) {
+        // Step forward one timestep
+        double[] currentStates = new double[bodies.size() * 6];
+        for (int i = 0; i < bodies.size(); i++) {
+            System.arraycopy(bodies.get(i).getState().getState(), 0, currentStates, i * 6, 6);
+        }
+
+        double[] nextStates = solver.RK4Step(time, currentStates, h);
+
+        for (int i = 0; i < bodies.size(); i++) {
+            double[] nextState = new double[6];
+            System.arraycopy(nextStates, i * 6, nextState, 0, 6);
+            bodies.get(i).setState(new State(time + h, nextState));
+        }
+        //ANCHOR THE SYSTEM TO THE SUN
+        CelestialObject sun = bodies.get(0);
+        double[] sunPos = sun.getState().getPos();
+        double[] sunVel = sun.getState().getVel();
+        //CHANGE SUN MOVEMENT TO ACCOMODAATE THAT MOVEMENT
+        for (CelestialObject obj : bodies) {
+            State old = obj.getState();
+            double[] newPos = vec.substract(old.getPos(), sunPos);
+            double[] newVel = vec.substract(old.getVel(), sunVel);
+            obj.setState(new State(old.getTime(), newPos, newVel));
+        }
+    }
+
+}
---
+++ src/main/java/Backend/Physics/SolarSystemODE.java
@@ -0,0 +1,55 @@
+package Backend.Physics;
+
+import Backend.SolarSystem.CelestialObject;
+import Utils.vec;
+
+import java.util.ArrayList;
+import java.util.function.BiFunction;
+
+public class SolarSystemODE {
+
+    public static BiFunction<Double, double[], double[]> create(ArrayList<CelestialObject> bodies) {
+        return (time, state) -> {
+            int n = bodies.size();
+            double[] derivatives = new double[n * 6]; // [x, y, z, vx, vy, vz] * n
+
+            for (int i = 0; i < n; i++) {
+                int idx = i * 6;
+
+                // Current body position and velocity
+                double[] pos_i = { state[idx], state[idx + 1], state[idx + 2] };
+                double[] vel_i = { state[idx + 3], state[idx + 4], state[idx + 5] };
+
+                // d(pos)/dt = velocity
+                derivatives[idx] = vel_i[0];
+                derivatives[idx + 1] = vel_i[1];
+                derivatives[idx + 2] = vel_i[2];
+
+                // d(vel)/dt = acceleration from all others
+                double[] acc = new double[3];
+
+                for (int j = 0; j < n; j++) {
+                    if (i == j) continue;
+
+                    int jdx = j * 6;
+                    double[] pos_j = { state[jdx], state[jdx + 1], state[jdx + 2] };
+                    double m_j = bodies.get(j).getMass();
+
+                    double[] r_ij = vec.substract(pos_j, pos_i);
+                    double dist = vec.magnitude(r_ij);
+                    if (dist == 0) continue;
+
+                    double G = 6.67430e-20;
+                    double scale = G * m_j / (dist * dist);
+                    acc = vec.add(acc, vec.multiply(vec.normalize(r_ij), scale));
+                }
+
+                derivatives[idx + 3] = acc[0];
+                derivatives[idx + 4] = acc[1];
+                derivatives[idx + 5] = acc[2];
+            }
+
+            return derivatives;
+        };
+    }
+}
